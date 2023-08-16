a5cb0289608efad304b12a2d14c03f70423e3b8a
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-08-16 15:49:50 +0200

    add basic folder structure, prettier, tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,18 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      neutral: {
+        100: '#fff',
+        200: '#fafafb',
+        400: '#cdd5e0',
+        700: '#677489',
+        900: '#111827',
       },
+      accent: '#3478f6',
+    },
+    fontFamily: {
+      sans: ['var(--font-lexend)'],
     },
   },
   plugins: [],
