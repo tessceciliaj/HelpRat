9668d7439eaf1098276519a2c4fc43985fcf9c7b
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-08-17 12:29:08 +0200

    Add HelpRat info to start page.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center">Hi</main>
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <article>
+        helpRat is a collaborative todo app that helps you stay organized and
+        manage your tasks effectively. Create, update, and delete tasks with an
+        intuitive and user-friendly interface.
+      </article>
+    </main>
+  )
 }
