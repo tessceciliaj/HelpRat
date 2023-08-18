cc3478427a64e079f215de1fff450ee0f1c9f690
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-08-18 13:49:17 +0200

    Add and position container divs. Temporary styling for visibility.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center">Hi</main>
+  return (
+    <div className="m-6 flex items-center justify-center">
+      <div className="max-w-md rounded bg-neutral-400 px-6 py-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Odio facilisis
+        mauris sit amet. Imperdiet proin fermentum leo vel orci porta. Tincidunt
+        vitae semper quis lectus nulla at. Eget aliquet nibh praesent tristique
+        magna sit. Euismod nisi porta lorem mollis aliquam ut porttitor leo a.
+        Ultricies lacus sed turpis tincidunt id aliquet risus. Viverra orci
+        sagittis eu volutpat. Vitae suscipit tellus mauris a diam maecenas. Sed
+        lectus vestibulum mattis ullamcorper velit sed ullamcorper. Massa enim
+        nec dui nunc mattis enim ut tellus elementum. Sed enim ut sem viverra
+        aliquet eget sit. Diam vulputate ut pharetra sit amet.
+      </div>
+    </div>
+  )
 }
