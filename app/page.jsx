40b57ea686d486a9e4ba8cd423075111cf04e7a0
Paola <paola.backstrom@gmail.com>
2023-08-18 13:41:46 +0200

    added header with logo and button

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
+      <Header />
       <div>HelpRat App</div>
       <Link href="/login">
         <button className="m-8 rounded-md bg-neutral-700 p-4 text-neutral-100 hover:bg-accent">
