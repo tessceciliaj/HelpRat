131aa2ea0b7992f16a7388645bdd547c7f9c1c19
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-08-21 14:48:53 +0200

    Remove space from body.

--- app/page.jsx
+++ app/page.jsx
@@ -19,10 +19,19 @@ export default async function Home() {
   ]
 
   return (
-    <main className="max-w-3xl">
+    <main className="w-full max-w-3xl">
       <Header />
+      {/* Temporary div, remove when necessary */}
       <div className="flex items-center justify-center">
-        <div className="max-w-3xl rounded bg-neutral-400 px-6 py-4">
+        <div>HelpRat App</div>
+        <Link href="/login">
+          <button className="m-8 rounded-md bg-neutral-700 p-4 text-neutral-100 hover:bg-accent">
+            Go to Login
+          </button>
+        </Link>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="rounded border-2 border-solid border-neutral-400 px-6 py-4">
           {mockTodos.map(todo => (
             <Todo key={todo.name} {...todo} />
           ))}
