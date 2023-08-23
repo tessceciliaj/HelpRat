94e8d09e1a873fde5f9ef4ea832118c568299717
Therese Cecilia Johansson <Theresececilias@gmail.com>
2023-08-23 11:26:44 +0200

    add switch view functionality on login page.

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -7,8 +7,6 @@ import { LogoBig } from '@/components/Logo'
 export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  // use this view to switch between views
-  // 'login', 'register', 'reset-password', 'check-email'
   const [view, setView] = useState('login')
 
   const router = useRouter()
@@ -24,6 +22,8 @@ export default function Login() {
       // handle error
       return
     }
+    // When successful registration
+    setView('check-email')
   }
 
   const login = async () => {
@@ -65,50 +65,91 @@ export default function Login() {
     }
   }
 
-  return (
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="pt-20">
-        <LogoBig />
-      </div>
-      <form>
-        <div className="relative mb-4">
-          <input
-            onChange={e => setEmail(e.target.value)}
-            type="email"
-            id="email"
-            placeholder="Email"
-            className="mt-16 w-full rounded border-2 border-neutral-400 p-2"
-          />
-          <label htmlFor="email" className="sr-only">
-            Email
-          </label>
-          <input
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-            id="password"
-            placeholder="Password"
-            className="my-2 w-full rounded border-2 border-neutral-400 p-2"
-          />
-          <label htmlFor="password" className="sr-only">
-            Password
-          </label>
-          <button className="w-full text-right text-sm font-semibold text-neutral-700">
-            Forgot password?
-          </button>
-        </div>
-        <div className="mb-12 flex flex-col pb-1 pt-1 text-center">
-          <button
-            onClick={e => handleOnSubmit(e)}
-            className="mb-2 rounded bg-neutral-900 p-4 font-semibold text-neutral-100">
-            Log in
-          </button>
-          <button type="button">
-            <p className="mb-0 mr-2 text-sm font-semibold text-neutral-700">
-              Don&apos;t have an account? Register
-            </p>
-          </button>
-        </div>
-      </form>
-    </main>
-  )
+  const switchToView = newView => {
+    setEmail('')
+    setPassword('')
+    setView(newView)
+  }
+
+   return (
+     <main className="flex min-h-screen flex-col items-center">
+       <div className="pt-20">
+         <LogoBig />
+       </div>
+       <form>
+         <div className="relative mb-4">
+           {view === 'reset-password' ? (
+             // Display only email input field for reset-password view
+             <input
+               onChange={e => setEmail(e.target.value)}
+               type="email"
+               id="email"
+               placeholder="Email"
+               className="mt-16 w-full rounded border-2 border-neutral-400 p-2"
+             />
+           ) : (
+             <>
+               <input
+                 onChange={e => setEmail(e.target.value)}
+                 type="email"
+                 id="email"
+                 placeholder="Email"
+                 className="mt-16 w-full rounded border-2 border-neutral-400 p-2"
+               />
+               {view !== 'check-email' && (
+                 <input
+                   onChange={e => setPassword(e.target.value)}
+                   type="password"
+                   id="password"
+                   placeholder="Password"
+                   className="my-2 w-full rounded border-2 border-neutral-400 p-2"
+                 />
+               )}
+               {/* Render "Forgot password?" link only for the login view */}
+               {view === 'login' && (
+                 <button
+                   type="button"
+                   onClick={() => setView('reset-password')}
+                   className="w-full text-right text-sm font-semibold text-neutral-700">
+                   Forgot password?
+                 </button>
+               )}
+             </>
+           )}
+         </div>
+         <div className="mb-12 flex flex-col pb-1 pt-1 text-center">
+           {view === 'check-email' ? (
+             <p className="mb-0 text-sm font-semibold text-neutral-700">
+               Thank you for registering! Please check your email to confirm.
+             </p>
+           ) : (
+             <>
+               <button
+                 onClick={e => handleOnSubmit(e)}
+                 className="mb-2 rounded bg-neutral-900 p-4 font-semibold text-neutral-100">
+                 {view === 'login'
+                   ? 'Log in'
+                   : view === 'register'
+                   ? 'Register'
+                   : 'Send Reset Link'}
+               </button>
+               <button
+                 type="button"
+                 onClick={() =>
+                   switchToView(view === 'register' ? 'login' : 'register')
+                 }>
+                 <p className="mb-0 mr-2 text-sm font-semibold text-neutral-700">
+                   {view === 'login'
+                     ? "Don't have an account? Register"
+                     : view === 'reset-password'
+                     ? "Don't have an account? Register"
+                     : 'Already have an account? Log in'}
+                 </p>
+               </button>
+             </>
+           )}
+         </div>
+       </form>
+     </main>
+   )
 }
